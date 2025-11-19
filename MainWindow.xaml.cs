f6542a569247e7e5dcaa28abeafe1b1054173f81
Miguel Reda Omail <147875415+Miguel-Reda@users.noreply.github.com>
2025-11-19 19:59:18 +0200

    Add MainWindow.xaml.cs with main window code-behind logic

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+
+namespace ClinicOS
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
