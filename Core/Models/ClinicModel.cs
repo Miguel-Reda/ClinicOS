31d4ff09238dd2c625d1b8630586a6d7b6335cb0
Miguel Reda Omail <147875415+Miguel-Reda@users.noreply.github.com>
2025-11-19 20:09:46 +0200

    Create ClinicModel class implementing MVVM

---
+++ Core/Models/ClinicModel.cs
@@ -0,0 +1,59 @@
+using System.ComponentModel;
+
+namespace ClinicOS.Core.Models
+{
+    public class ClinicModel : INotifyPropertyChanged
+    {
+        private int _id;
+        private string _name;
+        private string _code;
+        private string _address;
+
+        public int Id
+        {
+            get => _id;
+            set
+            {
+                _id = value;
+                OnPropertyChanged(nameof(Id));
+            }
+        }
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                _name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+
+        public string Code
+        {
+            get => _code;
+            set
+            {
+                _code = value;
+                OnPropertyChanged(nameof(Code));
+            }
+        }
+
+        public string Address
+        {
+            get => _address;
+            set
+            {
+                _address = value;
+                OnPropertyChanged(nameof(Address));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
